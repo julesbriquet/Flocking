befd3604e69de529bd952101d69582025578a56f
julesbriquet <jules.briquet@gmail.com>
2017-01-23 14:04:21 +0100

    Adding initial settup and scripts for flocking boid project
    Fly scene, boid prefab, and three simple script: a boid spawner, a boid
    movement, and a boid behavior that still need an implementation for
    cohesion, avoidance and alignment behavior.

---
+++ Assets/Scripts/BoidBehavior.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidBehavior : MonoBehaviour {
+
+	public bool 			m_DebugDrawBoidBehavior = false;
+
+	public float 			m_DistanceToConsidereNearbyBoids = 1.0f;
+	public float 			m_DesiredSeparationFromBoids = 0.5f;
+
+	public float 			m_AlignmentCoefficient = 1.0f;
+	public float 			m_CohesionCoefficient = 1.0f;
+	public float 			m_SeparationCoefficient = 1.0f;
+
+	private Vector3 		m_AligmentVector;
+	private Vector3 		m_CohesionVector;
+	private Vector3 		m_SeparationVector;
+	private Vector3 		m_SteeringVector;
+
+
+	private BoidSpawner 	m_FlightArea;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (m_FlightArea == null)
+		{
+			Debug.LogWarning ("BoidMovement::Start, Flight Area shoudn't be null");
+			return;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (m_FlightArea == null)
+		{
+			return;
+		}
+
+		List<GameObject> boidList = m_FlightArea.GetSpawnedEntityList ();
+
+		m_SeparationVector = GetSeparationFromBoidsVector (boidList);
+		m_AligmentVector = GetAlignmentFromBoidsVector (boidList);
+		m_CohesionVector = GetCohesionFromBoidsVector (boidList);
+
+		m_SeparationVector *= m_SeparationCoefficient;
+		m_AligmentVector *= m_AlignmentCoefficient;
+		m_CohesionVector *= m_CohesionCoefficient;
+
+		m_SteeringVector = m_SeparationVector + m_AligmentVector + m_CohesionVector;
+		m_SteeringVector.Normalize ();
+	}
+
+	private Vector3 GetSeparationFromBoidsVector(List<GameObject> _boidList)
+	{
+		// TODO
+		return Vector3.zero;
+	}
+
+	private Vector3 GetAlignmentFromBoidsVector(List<GameObject> _boidList)
+	{
+		// TODO
+		return Vector3.zero;
+	}
+
+	private Vector3 GetCohesionFromBoidsVector(List<GameObject> _boidList)
+	{
+		// TODO
+		return Vector3.zero;
+	}
+
+	public BoidSpawner FlightArea
+	{
+		get { return m_FlightArea; }
+		set { m_FlightArea = value; }
+	}
+
+	public Vector3 SteeringVector
+	{
+		get { return m_SteeringVector; }
+		set { m_SteeringVector = value; }
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		if (!m_DebugDrawBoidBehavior)
+		{
+			return;
+		}
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawLine (transform.position, transform.position + m_AligmentVector);
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine (transform.position, transform.position + m_CohesionVector);
+
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawLine (transform.position, transform.position + m_SeparationVector);
+
+		Gizmos.color = Color.magenta;
+		Gizmos.DrawLine (transform.position, transform.position + m_SteeringVector);
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere (transform.position, m_DistanceToConsidereNearbyBoids);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (transform.position, m_DesiredSeparationFromBoids);
+
+	}
+}
---
+++ Assets/Scripts/BoidMovement.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidMovement : MonoBehaviour {
+
+	public float 			m_BoidSpeed = 5.0f;
+
+	public bool 			m_DebugDrawBoidMouvementDebug = false;
+
+	private BoidSpawner 	m_FlightArea;
+	private Vector3 		m_TargetFlightDestination;
+	private float 			m_MinDistanceToDestinationRadius = 1.0f;
+
+	private Vector3 		m_TargetFlightDirection;
+	private Vector3 		m_SteeringFlightDirection;
+	private Vector3 		m_WantedFlightDirection;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (m_FlightArea == null)
+		{
+			Debug.LogWarning ("BoidMovement::Start, Flight Area shoudn't be null");
+			return;
+		}
+
+		m_FlightArea.GenerateTargetFlightDestination ();
+		SetTargetFlightDestination(m_FlightArea.TargetFlightDestination);
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		CheckTargetFlightDestination ();
+
+		m_TargetFlightDirection = m_TargetFlightDestination - transform.position;
+		m_TargetFlightDirection.Normalize ();
+
+		m_SteeringFlightDirection = GetSteeringBehaviorVector ();
+		m_SteeringFlightDirection.Normalize ();
+
+		m_WantedFlightDirection = m_TargetFlightDirection + m_SteeringFlightDirection;
+		m_WantedFlightDirection.Normalize ();
+
+		AdjustRotation ();
+
+		transform.position += m_BoidSpeed * Time.deltaTime * transform.forward;
+	}
+
+	private void CheckTargetFlightDestination()
+	{
+		Vector3 vDistanceToDestination = m_TargetFlightDestination - transform.position;
+
+		if (vDistanceToDestination.sqrMagnitude <= m_MinDistanceToDestinationRadius * m_MinDistanceToDestinationRadius)
+		{
+			m_FlightArea.GenerateTargetFlightDestination ();
+		}
+
+		SetTargetFlightDestination(m_FlightArea.TargetFlightDestination);
+	}
+
+	private void AdjustRotation ()
+	{
+		Quaternion targetRotation = Quaternion.LookRotation (m_WantedFlightDirection);
+		float factor = Time.deltaTime * m_BoidSpeed;
+
+		if (targetRotation != transform.rotation)
+		{
+			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation, factor);
+		}
+	}
+
+
+	private Vector3 GetSteeringBehaviorVector()
+	{
+		BoidBehavior boidBehavior = this.GetComponent<BoidBehavior> ();
+
+		if (boidBehavior == null)
+		{
+			return Vector3.zero;
+		}
+
+		return boidBehavior.SteeringVector;
+	}
+
+
+	//
+	//
+	// GETTER / SETTER
+	//
+	//
+
+	private void SetTargetFlightDestination(Vector3 _TargetFlightDestination)
+	{
+		m_TargetFlightDestination = _TargetFlightDestination;
+	}
+
+	public BoidSpawner FlightArea
+	{
+		get { return m_FlightArea; }
+		set { m_FlightArea = value;	}
+	}
+
+
+	//
+	//
+	// DEBUG PART
+	//
+	//
+	void OnDrawGizmosSelected()
+	{
+		if (!m_DebugDrawBoidMouvementDebug)
+		{
+			return;
+		}
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere (m_TargetFlightDestination, m_MinDistanceToDestinationRadius);
+
+		Gizmos.DrawLine (transform.position, transform.position + m_TargetFlightDirection);
+
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine (transform.position, transform.position + m_SteeringFlightDirection);
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine (transform.position, transform.position + m_WantedFlightDirection);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine (transform.position, transform.position + transform.forward);
+	}
+}
